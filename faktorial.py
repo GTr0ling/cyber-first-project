d73f8315f94f4203caa1727da8d6e0cffab18dbe
GTr0ling <kiryakinm@gmail.com>
2018-10-18 12:36:26 +0000

    Добавили к PY Vk.api

---
+++ faktorial.py
@@ -0,0 +1,94 @@
+n = int(input())
+if n < 0;
+    print ("Введите натуральное число")
+    else:
+        if 0 >= n and n >= 1:
+            print ("Факторил числа",n, "n+1")
+        else
+            sp = range (2,n+1)
+            result = 1
+    def factorial(n):
+        for i in sp
+            result = result*i
+        return result
+    print(factorial(n))
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ overfloat.py
@@ -0,0 +1,11 @@
+n = int(input())
+string = input()
+string.split()
+
+
+def mymax(3,5,6,9,12,2,4)
+    res = a [0]
+    for i in range(1, len(a))
+        if max < i;
+            max = i
+    return max
